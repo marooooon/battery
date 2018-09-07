26bade7ef66f07289ec9991bd520ed31a63bdb9a
Daijiro Takeuchi <takeuchi@tam-bourine.co.jp>
2018-09-07 15:28:31 +0900

    html,css,javascriptのコミット

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+var textForm1 = doucument.getElementById("textForm1").value;
+var textForm2 = doucument.getElementById("textForm1").value;
+
+var sum = parseFloat(textForm1) + parseFloat(textForm2);
+// var bunshi = ocv - guaranteeVolt;
